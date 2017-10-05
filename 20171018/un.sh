e5b075927c87fd9eecb7f78956dcdcdc15c2b2ea
alti-tude <ishaan.khare@research.iiit.ac.in>
2017-10-05 14:55:15 +0530

    IT lab uncompress

--- 20171018/un.sh
+++ 20171018/un.sh
@@ -2,14 +2,9 @@
 
 while [ true ]
 do
-	find ~/Downloads/ -name "*.tar" > fil
-	n=`wc -l < fil`
-
-	for ((i=1;i<=n;i++))
+	for arc in `find ~/Downloads -name "*.tar"`
 	do
-		arc=`head -n $i < fil | tail -n 1`
-		tar -xC ~/uncompressed/ -vf $arc
-				
+		tar -xC ~/uncompressed/ -vf $arc			
 		rm $arc
 	done
 done
