bc10c45750a1876afa8e0c2feb12e65ce9d04b63
saujasv <saujas1999@gmail.com>
2017-10-12 17:06:38 +0530

    Additional tasks (incomplete)

--- 20171098/todo.sh
+++ 20171098/todo.sh
@@ -40,6 +40,54 @@ function W {
 	done
 }
 
+function d {
+	while :
+	do
+		read -p "Are you sure you want to delete task $1? [Y/N]" choice
+		if [[ $choice = 'Y' ]]
+		then
+			touch temp
+			tasks=`cat tasks.txt | tr ' ' ':'`
+			n=1
+			for i in $tasks
+			do
+				if [ $n -ne $1 ]
+				then
+					echo "`echo $i | tr ':' ' '`" >> temp
+				fi
+				n=$(( n + 1 ))	
+			done
+			rm tasks.txt
+			mv temp tasks.txt
+			echo "Deleted task $1."
+			break
+		elif [[ $choice = 'N' ]]
+		then 
+			echo "Did not delete task."
+			break
+		fi
+	done
+}
+
+function c {
+	touch temp
+	tasks=`cat tasks.txt | tr ' ' ':'`
+	n=1
+	for i in $tasks
+	do
+		if [ $n -eq $1 ]
+		then
+			task_name=`cut -d':' -f1`
+			s=$task_name':0'
+			echo "`echo $s | tr ':' ' '`" >> temp
+		fi
+		n=$(( n + 1 ))	
+	done
+	rm tasks.txt
+	mv temp tasks.txt
+	echo "Marked task $1 as complete."
+}
+
 if [[ ! -e tasks.txt ]]
 then
 	touch tasks.txt
@@ -54,6 +102,7 @@ then
 			A `echo $2 | tr ' ' '_'` $3
 		else
 			echo "Priority of task must be 0 (complete), 1, 2, or 3."
+		fi
 	else
 		echo "Usage: bash todo.sh \"task name\" priority"
 	fi
@@ -66,6 +115,26 @@ then
 elif [[ $1 = '-P' ]]
 then
 	P
+elif [[ $1 = '-d' ]]
+then
+	task_no=$2
+	n_tasks=`wc -l tasks.txt | cut -d' ' -f1`
+	if [[ $task_no =~ ^[0-9]+$ && $task_no -le $n_tasks ]]
+	then
+		d $task_no
+	else
+		echo "Task number $2 does not exist."
+	fi
+elif [[ $1 = '-c' ]]
+then
+	task_no=$2
+	n_tasks=`wc -l tasks.txt | cut -d' ' -f1`
+	if [[ $task_no =~ ^[0-9]+$ && $task_no -le $n_tasks ]]
+	then
+		c $task_no
+	else
+		echo "Task number $2 does not exist."
+	fi
 else
 	echo "Usage: bash todo.sh -A [task-name]"
 	echo "       bash todo.sh -[T|P|W]"
