41273ec57e10eb4024437204fc2961902e7706ad
ajainuary <30972152+ajainuary@users.noreply.github.com>
2017-10-12 11:14:25 +0000

    Advanced Tasks
    Delete item
    delete list
    mark complete

--- 20171021/todo.sh
+++ 20171021/todo.sh
@@ -25,3 +25,28 @@ echo "-------COMPLETED TASKS-------"
 cat todo.txt | sort -nr|awk ' { t = $1; $1 = $3; $3 = t;if ($3 < 1)
  printf("%s:%s %s\n",NR,$2,$3); } '
 fi
+if [ "$1" == "-d"  ]
+then
+h=$2
+echo "Are you really want to delete `awk '{ if (NR =='$h') print;}' todo.txt` ? [Y/n]"
+read consent
+if [ "$consent" == "Y" ]
+then
+awk '{ if (NR != '$h' ) print;}' todo.txt > todo2.txt
+mv todo2.txt todo.txt
+fi
+fi
+if [ "$1" == "-D" ]
+then
+rm todo.txt
+touch todo.txt
+echo "0" > last.txt
+fi
+if [ "$1" == "-c" ]
+then
+h=$2
+awk '{ if (NR != '$h' ) print; else
+ printf("0 %s %s",$2,$3);}' todo.txt > todo2.txt
+mv todo2.txt todo.txt
+echo "marking task $h completed"
+fi
